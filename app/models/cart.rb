0092508d25c3cdb1a519b8191b2d844cd67f316a
sbstn <sbstn.jmnz@yahoo.com>
2015-05-01 21:23:31 -0300

    CartModel1

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,19 +1,29 @@
 class Cart
+	#esta no es una clase Active Record. Es una simple clase ruby
+	#El la Clase Cart tendra items
 	attr_reader :items
 
+	# El constructor de la clase creara para cada instancia una variable de instancia @items  
 	def initialize
 		@items = []
 	end
 	
+	#1- se escribe el metodo add_item para pasar la prueba
+	#2- este metodo agregara producos al carro, pero si el producto ya 
+	#existe, entonces no lo agrega
 	def add_item product_id
+		#primero se busca el item
 		item = @items.find { |item| item.product_id == product_id }
+		#si hay algo en el carro, se incremente
 		if item
 		item.increment
+		#sino se crea un carro y se pasa el product_id
 		else	
 			@items << CartItem.new(product_id)
 		end
 	end
 
+	# se escribe el metodo empty
 	def empty?
 		@items.empty?
 	end
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,21 +1,28 @@
 require "test_helper"
 
-class CartTest < MiniTest::Test
-	def test_adds_one_item
-		cart = Cart.new
-		cart.add_item 1
+class CartTest < MiniTest::Test	
 
-		assert_equal cart.empty?, false
+	#Se define una prueba como un metodo de la clase CartTest
+	def test_adds_one_item # prueba_agrega_un_item
+		cart = Cart.new # se instancia un nuevo carrito
+		cart.add_item 1 # al carro creado se le llama el metodo add_item y se le pasa un entero, que seria un id
+		#finalmente se hace la prueba
+		assert_equal cart.empty?, false #Esta el carro vacio?, la respuesta debiera ser falso si salio todo bien
+		#hay que escribir el metodo empty
 	end
+	#La primera vez la prueba va a fallar, porque no se ha programado un metodo add_item a la clase 
+	#Cart
 
+	#esto es para agregar varias veces el mismo producto
 	def test_adds_up_in_quanttity
 		cart = Cart.new
 		3.times { cart.add_item 1 }
 
-		assert_equal cart.items.length, 1
-		assert_equal cart.items.first.quantity, 3
+		assert_equal cart.items.length, 1 #solo un item
+		assert_equal cart.items.first.quantity, 3 #varias unidades del mismo item
 	end
 
+	#
 	def test_retrieves_products
 		product = Product.create! name: "Pepinillo", price: 1
 		cart = Cart.new
